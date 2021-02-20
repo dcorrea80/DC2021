ed4c2062956c11366e581cc844f97b2a4ee92f6d
David Correa <dcorrea80@gmail.com>
2021-02-19 22:14:31 -0600

    Testing rebuild

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'David'
+const name = 'DC2021'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
